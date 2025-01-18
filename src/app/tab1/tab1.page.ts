31b0a75353c1ccffc783579d470ff07ffa82bbda
Kragan0 <alex.geleta11@gmail.com>
2025-01-18 19:48:19 +0100

    Changed names of path in routing, changed icons, added input form for ingredients

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,7 +7,29 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
-
   constructor() {}
+    newIngredient: string = '';
+    ingredients: string[] = [];
+
+  addIngredient() {
+    if (this.newIngredient.trim()) 
+      this.ingredients.push(this.newIngredient.trim());
+      this.newIngredient = '';
+  }
+
+  removeIngredient() {
+    if (this.ingredients.length > 0)
+      this.ingredients.pop();
+    else
+    {
+      console.log('No ingredients to remove');
+    }
+  }
+  onSubmit() {
+    if (this.ingredients.length > 0)
+    {
+      console.log(this.ingredients);
+    }
+  }
 
 }
